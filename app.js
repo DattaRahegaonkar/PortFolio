99dc30b1ddbf2ab463cfb47ffa550a73a127d564
Datta Rahegaonkar <rahegaonkardatta@gmail.com>
2024-09-16 13:35:51 +0530

    add files

---
+++ app.js
@@ -0,0 +1,62 @@
+
+//   hamburger menu
+
+let sidemenu = document.querySelector(".sidemenu");
+let openmenu = document.querySelector(".openmenu");
+let closemenu = document.querySelector(".closemenu");
+let ClimateLink = document.querySelector(".Climate-link");
+let PortFolioLink = document.querySelector(".PortFolio-link");
+let StayHavenLink = document.querySelector(".StayHaven-link");
+let StayHavenLive = document.querySelector(".StayHaven-live");
+let ClimateLive = document.querySelector(".Climate-live");
+let PortFolioLive = document.querySelector(".PortFolio-live");
+
+openmenu.addEventListener("click", ()=> {
+    sidemenu.style.right = "0";
+})
+
+closemenu.addEventListener("click", ()=> {
+    sidemenu.style.right = "-200px";
+})
+
+let Home = document.querySelector(".Home");
+let About = document.querySelector(".About");
+let Resume = document.querySelector(".Resume");
+let Projects = document.querySelector(".Projects");
+let Contacts = document.querySelector(".Contacts");
+
+let navli = document.querySelector("nav li");
+
+let arrtab = [Home, About, Resume, Projects, Contacts];
+
+arrtab.forEach(tab => {
+    tab.addEventListener("click", () => {
+        sidemenu.style.right = "-200px";
+        navli.style.color = "#fff";
+    });
+});
+
+
+ClimateLink.addEventListener("click", () => {
+    window.open('https://github.com/DattaRahegaonkar/Climate-Cast','_black');
+})
+
+ClimateLive.addEventListener("click", () => {
+    window.open('https://github.com/DattaRahegaonkar/Climate-Cast','_black');
+})
+
+PortFolioLink.addEventListener("click", () => {
+    window.open('https://github.com/DattaRahegaonkar/PortFolio','_black');
+})
+
+PortFolioLive.addEventListener("click", () => {
+    window.open('https://dattarahegaonkarportfolio.netlify.app/','_black');
+})
+
+StayHavenLink.addEventListener("click", () => {
+    window.open('https://github.com/DattaRahegaonkar/StayHaven','_black');
+})
+
+StayHavenLive.addEventListener("click", () => {
+    window.open('https://stayhaven-u6tg.onrender.com/stayhaven','_black');
+})
---
+++ app2.js
@@ -0,0 +1,58 @@
+ScrollReveal({
+     // reset: true,
+     distance: '30px',
+     duration: 1000,
+     delay: 400
+});
+
+ScrollReveal().reveal('.main-head-2, .contact-head', { delay: 200, origin: 'bottom' });
+ScrollReveal().reveal('.main-head-1', { delay: 400, origin: 'bottom' });
+ScrollReveal().reveal('.front', { delay: 100, origin: 'bottom' });
+
+ScrollReveal().reveal('.about-head', { delay: 100, origin: 'bottom' });
+ScrollReveal().reveal('.about-image', { delay: 300, origin: 'bottom' });
+ScrollReveal().reveal('.about-text', { delay: 500, origin: 'bottom' });
+ScrollReveal().reveal('.resume', { delay: 300, origin: 'bottom' });
+
+ScrollReveal().reveal('.project-heading', {  delay: 100, origin: 'bottom' });
+ScrollReveal().reveal('.project-1', { delay: 300, origin: 'bottom' });
+ScrollReveal().reveal('.project-2', { delay: 500, origin: 'bottom' });
+ScrollReveal().reveal('.project-3', { delay: 700, origin: 'bottom' });
+ScrollReveal().reveal('.project-4', { delay: 900, origin: 'bottom' });
+
+ScrollReveal().reveal('.num-em', {
+     distance: '30px',
+     interval: 400,
+     delay: 600,
+     origin: 'bottom',
+});
+
+ScrollReveal().reveal('.social-logo a', {
+     distance: '10px',
+     delay: 600,
+     origin: 'bottom'
+ });
+
+ScrollReveal().reveal('.logo i', {
+     distance: '20px',
+     delay: 200,
+     interval: 300,
+     origin: 'bottom',
+ });
+
+
+ ScrollReveal().reveal('.copyright', {
+     distance: '0',
+     delay: 800,
+     origin: 'bottom',
+});
+
+ScrollReveal().reveal('hr', {
+     distance: '10px',
+     delay: 700,
+     origin: 'bottom',
+});
+
+
+
+
